3e83609040b6aa1294a5db95c5f55d96bed53f63
RevDonz <aprilio842@gmail.com>
2022-04-02 10:14:00 +0700

    Menambahkan Class DataMahasiswa1302204051

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul6_kelompok_04;
+
+internal class MainClass
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa1302204051 mhs1 = new DataMahasiswa1302204051();
+        mhs1.readJSON();
+    }
+}

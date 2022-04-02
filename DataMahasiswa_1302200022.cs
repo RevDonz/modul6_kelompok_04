66dd1f4c95bf4ca924a7de137de0d1a7a491b1da
naufalchairurrijal <naufalalifaza@student.telkomuniversity.ac.id>
2022-04-02 10:45:23 +0700

    menambahkan class data mahasiswa

---
+++ DataMahasiswa_1302200022.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace modul6_kelompok_04
+{
+    internal class DataMahasiswa_1302200022
+    {
+
+
+        public void ReadJson()
+        {
+            // Membaca File JSON
+            string jsonString = File.ReadAllText("C:/Users/HP/Documents/KPL/KPL_MOD6_1302200022_RFL/modul6_kelompok_04/jurnal6_1_1302200022.json");
+
+            // Convert JSON menjadi Array
+            dynamic data = JsonConvert.DeserializeObject(jsonString);
+
+            //
+            Console.WriteLine(data);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using modul6_kelompok_04;
+
 Console.WriteLine("Hello, World!");
+
+DataMahasiswa_1302200022 mhs1 = new DataMahasiswa_1302200022();
+mhs1.ReadJson();
+

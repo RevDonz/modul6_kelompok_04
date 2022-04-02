ffd6d16c3dd02fdc2d9da8a1b0ec627e75bc435c
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-04-02 11:00:06 +0700

    Menambahkan Kelas TeamMembers_1302204080

--- Program.cs
+++ Program.cs
@@ -10,15 +10,36 @@ namespace Jurnal6
     {
         public static void Main(string[] args)
         {
-            DataMahasiswa_1302204080 objhasilbaca = ReadJSON();
+            DataMahasiswa_1302204080 objhasilbaca = ReadJSON1();
             Console.WriteLine(objhasilbaca.firstName);
+            Console.WriteLine(objhasilbaca.lastName);
+            Console.WriteLine(objhasilbaca.age);
+            Console.WriteLine(objhasilbaca.gender);
+            Console.WriteLine(objhasilbaca.address.city);
+            Console.WriteLine(objhasilbaca.address.streetAddress);
+            Console.WriteLine(objhasilbaca.address.state);
+            Console.WriteLine(objhasilbaca.courses[0].name);
+            Console.WriteLine(objhasilbaca.courses[0].code);
+            Console.WriteLine(objhasilbaca.courses[1].name);
+            Console.WriteLine(objhasilbaca.courses[1].code);
+
+            TeamMembers_1302204080 objhasilread = ReadJSON2();
+
+
         }
 
-        static DataMahasiswa_1302204080 ReadJSON()
+        static DataMahasiswa_1302204080 ReadJSON1()
         {
-            string jsonstring = File.ReadAllText("D:\\Project Koding Kuliah\\KPL\\jurnalmodul6\\modul6_kelompok_04\\tp6_1_1302204080.json");
+            string jsonstring = File.ReadAllText("D:\\Project Koding Kuliah\\KPL\\jurnalmodul6\\modul6_kelompok_04\\jurnal6_1_1302204080.json");
             DataMahasiswa_1302204080 objHasilDeserialisasi = JsonSerializer.Deserialize<DataMahasiswa_1302204080>(jsonstring);
             return objHasilDeserialisasi;
+        }
+
+        static TeamMembers_1302204080 ReadJSON2()
+        {
+            string jsonstring = File.ReadAllText("D:\\Project Koding Kuliah\\KPL\\jurnalmodul6\\modul6_kelompok_04\\jurnal6_2_1302204080.json");
+            TeamMembers_1302204080 objHasilDeserialisasi2 = JsonSerializer.Deserialize<TeamMembers_1302204080>(jsonstring);
+            return objHasilDeserialisasi2;
         }
 
     }
---
+++ TeamMembers_1302204080.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal6
+{
+    public class TeamMembers_1302204080
+    {
+        public List<members> Members { get; set; }
+        public TeamMembers_1302204080() { }
+
+    }
+
+    public class members
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+
+        public members() { }
+
+    }
+
+}

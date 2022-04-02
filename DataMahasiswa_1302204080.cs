b57cf01352752a2c6dab73d3f2d2956f25bfd8d1
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-04-02 10:21:54 +0700

    Menambahkan class Data_Mahasiswa_1302204080, beserta class yang bersangkutan

---
+++ DataMahasiswa_1302204080.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal6
+{
+    public class DataMahasiswa_1302204080
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public address address { get; set; }
+        public List<courses> courses { get; set; }
+
+
+
+        public DataMahasiswa_1302204080() { }
+        public DataMahasiswa_1302204080(string nama1, string nama2, string kelamin, int umur)
+        {
+            firstName = nama1;
+            lastName = nama2;   
+            gender = kelamin;
+            age = umur;
+        }
+
+
+
+    }
+
+    public class address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+        public address() { }
+    }
+
+    public class courses
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+
+        public courses() { }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Text.Json;
+using System.IO;
+
+
+namespace Jurnal6
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            DataMahasiswa_1302204080 objhasilbaca = ReadJSON();
+            Console.WriteLine(objhasilbaca.firstName);
+        }
+
+        static DataMahasiswa_1302204080 ReadJSON()
+        {
+            string jsonstring = File.ReadAllText("D:\\Project Koding Kuliah\\KPL\\jurnalmodul6\\modul6_kelompok_04\\tp6_1_1302204080.json");
+            DataMahasiswa_1302204080 objHasilDeserialisasi = JsonSerializer.Deserialize<DataMahasiswa_1302204080>(jsonstring);
+            return objHasilDeserialisasi;
+        }
+
+    }
+}

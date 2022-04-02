6743e9f5a66b9b37898d1796a822b78019cba844
rpadliwinata <rpadliwinata@student.telkomuniversity.ac.id>
2022-04-02 10:33:31 +0700

    menambahkan class DataMahasiswa1302200028

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Mod6
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataMahasiswa1302200028 data = new DataMahasiswa1302200028();
+            data.ReadJson();
+        }
+    }
+
+}
